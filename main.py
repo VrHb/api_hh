a45e253176c1600c6e2cc6e8695ee417a4cd5203
Victor Abdukarimov <viredmi@gmail.com>
2022-06-11 02:33:30 +0300

    Added func for sj

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import requests
 
 
 HH_API_URL: Final = "https://api.hh.ru/vacancies"
-
+SJ_API_URL: Final = "https://api.superjob.ru/2.0/vacancies"
 
 def get_access_token(oauth_data: dict):
     response = requests.post(
@@ -17,19 +17,19 @@ def get_access_token(oauth_data: dict):
     return response.json()
 
 
-def get_vacancies_info(hh_api_params: dict) -> None:
+def get_hh_vacancies_info(hh_api_params: dict) -> None:
     response = requests.get(HH_API_URL, params=hh_api_params)
     response.raise_for_status()
     return response.json()
 
 
-def get_found_vacancies(hh_api_params: dict) -> None:
+def get_hh_found_vacancies(hh_api_params: dict) -> None:
     response = requests.get(HH_API_URL, params=hh_api_params)
     response.raise_for_status()
     return response.json()["found"]
 
 
-def get_vacancies_payment_range(hh_api_params: dict) -> list:
+def get_hh_vacancies_payment_range(hh_api_params: dict) -> list:
     vacancies_salary = []
     page = 0
     page_number = 1
@@ -59,7 +59,7 @@ def predict_rub_salary(vacancy: dict) -> float | None:
         return None
 
 
-def get_average_salary(vacancies_salary: list) -> int:
+def get_average_hh_salary(vacancies_salary: list) -> int:
     salary = 0
     for vacancy_salary in vacancies_salary:
         if predict_rub_salary(vacancy_salary):
@@ -67,16 +67,34 @@ def get_average_salary(vacancies_salary: list) -> int:
     return int(salary / len(vacancies_salary))
 
 
-def get_vacancies_sj():
-    response = requests.get(
-        url="https://api.superjob.ru/2.0/vacancies",
-        headers={"X-Api-App-Id": os.getenv("SJ_API_KEY")},
-        params={"town": "Москва", "keyword": "программист"}
+def get_vacancies_from_sj(params: dict, headers: dict) -> list:
+    sj_vacancies_total = []
+    page = 0
+    page_number = 1
+    while page < page_number:
+        params["page"] = page
+        page_response = requests.get(
+            url=SJ_API_URL,
+            headers=headers,
+            params=params
 
-    )
-    response.raise_for_status()
-    return response.json()["objects"]
-    
+        )
+        page_response.raise_for_status()
+        for item in page_response.json()["objects"]:
+            sj_vacancies_total.append(item)
+        page_number = int(page_response.json()["total"] / 20)
+        page += 1
+    return sj_vacancies_total
+ 
+
+def get_average_sj_salary(vacancies_salary: list) -> int:
+    salary = 0
+    for vacancy_salary in vacancies_salary:
+        if predict_rub_salary_for_sj(vacancy_salary):
+            salary += predict_rub_salary_for_sj(vacancy_salary)
+    return int(salary / len(vacancies_salary))
+
+  
 
 def predict_rub_salary_for_sj(vacancy: dict) -> float | None:
     if vacancy["currency"] == "rub":
@@ -96,7 +114,8 @@ def predict_rub_salary_for_sj(vacancy: dict) -> float | None:
 def main():
     load_dotenv()
 
-    """python_vacancies = get_found_vacancies(
+    """
+    python_vacancies = get_found_vacancies(
         {
             "specialization": 1.221,
             "area": 1,
@@ -190,7 +209,7 @@ def main():
         }
     )
     
-    vacancies_for_petia = {
+    vacancies_hh_for_petia = {
         "python": {
             "vacancies_found": python_vacancies,
             "vacancies_processed": len(python_vacancies_payments),
@@ -212,11 +231,55 @@ def main():
     print(vacancies_for_petia)
 """
 
-    vacancies_sj = get_vacancies_sj()
-    for item in vacancies_sj:
-        # print(item)
+    vacancies_sj_moskow = get_vacancies_from_sj(
+        headers={"X-Api-App-Id": os.getenv("SJ_API_KEY")},
+        params={
+            "town": "Москва",
+            "keyword": "программист"
+        }
+    )
+    for item in vacancies_sj_moskow:
         print("-------------------------------------------------------------")
         print(f'{item["profession"]}, {item["town"]["title"]}, {predict_rub_salary_for_sj(item)}')
+
+
+    vacancies_sj_python = get_vacancies_from_sj(
+        headers={"X-Api-App-Id": os.getenv("SJ_API_KEY")},
+        params={
+            "keyword": "программист python",
+        }
+    )   
+    vacancies_sj_java = get_vacancies_from_sj(
+        headers={"X-Api-App-Id": os.getenv("SJ_API_KEY")},
+        params={
+            "keyword": "программист java",
+        }
+    )   
+    
+
+    print("-------------------------------------------------------------")
+    vacancies_sj_for_petia = {
+        "Python": {
+            "vacancies_found": len(vacancies_sj_python),
+            "vacancies_processed": len(vacancies_sj_python),
+            "average_salary": get_average_sj_salary(vacancies_sj_python)
+        },
+        "Java": {
+            "vacancies_found": len(vacancies_sj_java),
+            "vacancies_processed": len(vacancies_sj_java),
+            "average_salary": get_average_sj_salary(vacancies_sj_java)
+        }
+    }
+    print(vacancies_sj_for_petia)
+    print("-------------------------------------------------------------")
+
+"""
+Нужно:
+1. Убрать, объединить лишние функции
+2. Разобраться с обработанными вакансиями, это выходит те, что оплачиваются в
+рублях
+3. Сделать вывод красивый
+"""
 
 if __name__ == "__main__":
     main()

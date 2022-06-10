7de7e71f1df75c10ac9bdb7b023a1ce88b9b4e18
Victor Abdukarimov <viredmi@gmail.com>
2022-06-10 11:24:12 +0300

    Added func

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
 import json
+from typing import Final
 
 import requests
 
 
+HH_API_URL: Final = "https://api.hh.ru/vacancies"
+
+
+def get_access_token(oauth_data: dict):
+    response = requests.post(
+        url="https://hh.ru/oauth/token",
+        data=oauth_data
+    )
+    return response.json()
+
+
+def get_vacancy(hh_api_params: dict) -> None:
+    response = requests.get(HH_API_URL, params=hh_api_params)
+    response.raise_for_status()
+    print(response.json())
+
+
 def main():
-    def get_vacancy(vacancy_type: str) -> None:
-        pass
+    get_vacancy({"specialization": 1.221})
 
 
+
+if __name__ == "__main__":
+    main()

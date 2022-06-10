3661f42fa46af06abf5321a93e65f890e333e6e4
Victor Abdukarimov <viredmi@gmail.com>
2022-06-10 13:51:18 +0300

    Added functions

--- main.py
+++ main.py
@@ -15,15 +15,113 @@ def get_access_token(oauth_data: dict):
     return response.json()
 
 
-def get_vacancy(hh_api_params: dict) -> None:
+def get_vacancies_info(hh_api_params: dict) -> None:
     response = requests.get(HH_API_URL, params=hh_api_params)
     response.raise_for_status()
-    print(response.json())
+    return response.json()
+
+
+def get_found_vacancies(hh_api_params: dict) -> None:
+    response = requests.get(HH_API_URL, params=hh_api_params)
+    response.raise_for_status()
+    return response.json()["found"]
+
+
+def get_payment_range(hh_api_params: dict) -> list:
+    response = requests.get(HH_API_URL, params=hh_api_params)
+    response.raise_for_status()
+    vacancies_salary = []
+    for item in response.json()["items"]:
+         vacancies_salary.append(item["salary"])
+    return vacancies_salary
 
 
 def main():
-    get_vacancy({"specialization": 1.221})
-
+    python_vacancies = get_found_vacancies(
+        {
+        "specialization": 1.221,
+        "area": 1,
+        "period": 30,
+        "text": "Программист Python"
+        }
+    )
+    javascript_vacancies = get_found_vacancies(
+        {
+        "specialization": 1.221,
+        "area": 1,
+        "period": 30,
+        "text": "Программист javascript"
+        }
+    )
+    ruby_vacancies = get_found_vacancies(
+        {
+        "specialization": 1.221,
+        "area": 1,
+        "period": 30,
+        "text": "Программист ruby"
+        }
+    )
+    java_vacancies = get_found_vacancies(
+        {
+        "specialization": 1.221,
+        "area": 1,
+        "period": 30,
+        "text": "Программист java"
+        }
+    )
+    php_vacancies = get_found_vacancies(
+        {
+        "specialization": 1.221,
+        "area": 1,
+        "period": 30,
+        "text": "Программист php"
+        }
+    )
+    cplus_vacancies = get_found_vacancies(
+        {
+        "specialization": 1.221,
+        "area": 1,
+        "period": 30,
+        "text": "Программист c++"
+        }
+    )
+    c_vacancies = get_found_vacancies(
+        {
+        "specialization": 1.221,
+        "area": 1,
+        "period": 30,
+        "text": "Программист c"
+        }
+    )
+    csharp_vacancies = get_found_vacancies(
+        {
+        "specialization": 1.221,
+        "area": 1,
+        "period": 30,
+        "text": "Программист c#"
+        }
+    )
+    top_vacancies = {
+        "python": python_vacancies,
+        "Java": java_vacancies,
+        "Javascript": javascript_vacancies,
+        "PHP": php_vacancies,
+        "C#": csharp_vacancies,
+        "C++": cplus_vacancies,
+        "RUBY": ruby_vacancies,
+        "C": c_vacancies
+    }
+    
+    python_vacancies_payment = get_payment_range(
+        {
+            "text": "программист python",
+            "salary": 180000,
+            "curency": "RUR"
+        }
+    )
+    
+    print(top_vacancies)
+    print(python_vacancies_payment)
 
 
 if __name__ == "__main__":
